d4e5143e607c317911f40ba12b4ea7ce42e9ee82
matzac <zacniewski@gmail.com>
2017-01-24 23:37:17 +0100

    Sterowanie kursorem

--- BattleshipsAI/BattleshipsAI.cpp
+++ BattleshipsAI/BattleshipsAI.cpp
@@ -2,11 +2,51 @@
 //
 
 #include "stdafx.h"
+#include "conio.h"
+#include "iostream"
+#include <Windows.h>
+using namespace std;
 
+int x = 0;
+int y = 0;
 
-int _tmain(int argc, _TCHAR* argv[])
+void gotoxy(int x, int y)
 {
+	COORD coord;              
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); 
+}
 
+void sterowanie(char znak){
+	const int GORA = 72, DOL = 80, LEWO = 75, PRAWO = 77, ENTER = 13, ESC = 27;
+	switch (znak){
+	case GORA: {
+		if (y > 0)
+			gotoxy(x, --y);
+		break;
+	}
+	case DOL: {
+		gotoxy(x, ++y);
+		break;
+	}
+	case LEWO: {
+		if (x > 0)
+			gotoxy(--x, y);
+		break;
+	}
+	case PRAWO: {
+		gotoxy(++x, y);
+		break;
+	}
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[]){
+	do {
+		sterowanie(_getch());
+	} while (1);
+	_getch();
 	return 0;
 }
 
