fc60d88a4e26cfc80279cd2a663fa4c5e720d520
matzac <zacniewski@gmail.com>
2017-01-25 19:45:21 +0100

    Ustawianie statkow przez komputer v 1.0

--- BattleshipsAI/BattleshipsAI.cpp
+++ BattleshipsAI/BattleshipsAI.cpp
@@ -6,11 +6,16 @@
 #include "iostream"
 #include <Windows.h>
 #include "vector"
+#include <ctime>
+#include <stdlib.h>
+#include <stdio.h> 
 using namespace std;
 
 int x=1;
 int y=1;
 const int GORA = 72, DOL = 80, LEWO = 75, PRAWO = 77, ENTER = 13, ESC = 27;
+
+
 
 void gotoxy(int x, int y)
 {
@@ -34,7 +39,6 @@ char getxy(int x, int y)
 	}
 	return c;
 }
-
 
 void sterowanie(char znak){
 	
@@ -149,13 +153,19 @@ public:
 						cout << "blad";
 					}
 					else {
-						if (getxy(p.back().x, p.back().y) == getxy(x + 1, y) || getxy(p.back().x, p.back().y) == getxy(x - 1, y) ||
-							getxy(p.back().x, p.back().y) == getxy(x, y + 1) || getxy(p.back().x, p.back().y) == getxy(x, y - 1)){
-							gotoxy(x, y);
-							this->p.push_back(Punkt(x, y));
-							maszty--;
-							gotoxy(x, y);
-							break;
+						if (p.size() != 0){
+							if (getxy(p.back().x, p.back().y) == getxy(x + 1, y) || getxy(p.back().x, p.back().y) == getxy(x - 1, y) ||
+								getxy(p.back().x, p.back().y) == getxy(x, y + 1) || getxy(p.back().x, p.back().y) == getxy(x, y - 1)){
+								gotoxy(x, y);
+								this->p.push_back(Punkt(x, y));
+								maszty--;
+								gotoxy(x, y);
+								break;
+							}
+							else {
+								gotoxy(30, 1);
+								cout << "blad";
+							}
 						}
 						else {
 							gotoxy(30, 1);
@@ -193,6 +203,67 @@ class Plansza{
 public:
 	int wynik[10][10];
 
+
+	Plansza(){
+
+		for (int i = 0; i < 10; i++){
+			for (int j = 0; j < 10; j++){
+				wynik[i][j] = 0;
+			}
+		}
+
+		int yr;
+		int xr;
+		bool poziomo;
+		int ship[10] = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+		gotoxy(0, 20);
+		for (int i = 0; i < 10; i++){
+			do{
+				xr = (rand() % 10);
+				yr = (rand() % 10);
+				yr = yr % 10;
+				if (rand() % 2){
+					//poziomo
+					if (10 - xr >= ship[i]){
+						do{
+							wynik[yr][xr] = 1;
+							xr++;
+							ship[i]--;
+						} while (ship[i] != 0);
+					}
+
+					else {
+						cout << "blad: " << xr << " " << yr << endl;
+					}
+				}
+
+				else {
+					//pionowo
+					if (10 - yr >= ship[i]){
+						do{
+							wynik[yr][xr] = 1;
+							yr++;
+							ship[i]--;
+						} while (ship[i] != 0);
+					}
+					else {
+						cout << "blad: " << xr << " " << yr << endl;
+					}
+				}
+			} while (ship[i] != 0);
+			cout << "PETLA " << i << endl;
+			//_getch();
+		}
+
+		for (int i = 0; i < 10; i++){
+			for (int j = 0; j < 10; j++){
+				cout << wynik[i][j];
+			//	_getch();
+			}
+			cout << endl;
+		}
+	}
+
 	Plansza(vector<Statek> s){
 		int dx;
 		int dy;
@@ -213,9 +284,13 @@ public:
 		}
 	}
 
+
 	void show(int y){
+		cout << "WCHODZE";
+		gotoxy(0, 0);
 		rysujPlansze();
 		for (int i = 0; i < 10; i++){
+			
 			gotoxy(y, i+1);
 			for (int j = 0; j < 10; j++){
 				if (wynik[i][j] == 0){
@@ -233,7 +308,8 @@ public:
 int Statek::count = 0;
 
 int _tmain(int argc, _TCHAR* argv[]){
-	int m1 = 4;
+	srand(time(NULL));
+/*	int m1 = 4;
 	int m2 = 3;
 	int m3 = 2;
 	int m4 = 1;
@@ -254,7 +330,7 @@ int _tmain(int argc, _TCHAR* argv[]){
 		
 
 		cout << "Pozostale statki do rozstawienia: " << endl;
-		cout << "1-masztowe: " << m1 << endl;
+		cout << "1-masztowe: " << m1 <<  endl;
 		cout << "2-masztowe: " << m2 << endl;
 		cout << "3-masztowe: " << m3 << endl;
 		cout << "4-masztowe: " << m4 << endl;
@@ -302,8 +378,12 @@ int _tmain(int argc, _TCHAR* argv[]){
 	Plansza plansza = Plansza(ships);
 	plansza.show(1);
 	gotoxy(0, 0);
+	*/
+
+	
+
+	Plansza komp = Plansza();
+	komp.show(1);
 	_getch();
-
-
 	return 0;
 }
